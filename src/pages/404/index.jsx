2548cf3c2985f0d9ebf4489bcb260cbe980b85db
Anthony Chanty <anthony.chanty@hotmail.com>
2022-12-22 20:53:06 +0100

    add 404 page content

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom'
+
 function NotFound() {
   return (
     <div>
-      <h1>Oups 🙈 Cette page n'existe pas</h1>
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <p>
+        <Link to="/">Retourner sur la page d'accueil</Link>
+      </p>
     </div>
   )
 }
