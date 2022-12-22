43576c6649f6adce2217db803815b0735259d810
Anthony Chanty <anthony.chanty@hotmail.com>
2022-12-22 20:25:19 +0100

    feat create react routers routes

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <nav>
+      <Link to="/">Accueil</Link>
+      <Link to="/about">À propos</Link>
+    </nav>
+  )
+}
+
+export default Header
