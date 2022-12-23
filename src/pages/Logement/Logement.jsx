405a36c2526139fed23347643326c074b3e8b60f
Anthony Chanty <anthony.chanty@hotmail.com>
2022-12-23 21:33:39 +0100

    feat add logements.jsx content

--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -1,11 +1,46 @@
-import { Link } from 'react-router-dom'
 import './logement.css'
 import Logements from './../../assets/datas/logements.json'
+import { useParams } from 'react-router-dom'
 
 function Logement() {
+   const { id } = useParams() // utilise l'id de l'url
+   const logement = Logements.find((element) => {
+      // cherche l'id de l'url dans logements.json pour faire correspondre la page au logement
+      return element.id === id
+   })
+
+   const {
+      cover,
+      title,
+      location,
+      tags,
+      rating,
+      host,
+      description,
+      equipments,
+   } = logement
    return (
       <div>
-         <h1>test</h1>
+         <img src={cover} alt={title} />
+         <h1>{title}</h1>
+         <p>{location}</p>
+         <ul>
+            {tags.map((tags) => {
+               return <li key={tags}>{tags}</li>
+            })}
+         </ul>
+         <p>{rating}</p>
+         <p>{host.name}</p>
+         <img src={host.picture} alt={host.name} />
+
+         <h3>Description</h3>
+         <p>{description}</p>
+         <h3>Equipements</h3>
+         <ul>
+            {equipments.map((equipments) => {
+               return <li key={equipments}>{equipments}</li>
+            })}
+         </ul>
       </div>
    )
 }
