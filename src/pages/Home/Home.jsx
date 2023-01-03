55b0e4403cd72a0798557e5450fc6b0a6f2767fc
Anthony Chanty <anthony.chanty@hotmail.com>
2023-01-03 16:01:12 +0100

    feat responsive grid home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
 import './home.css'
 import Banner from '../../components/Banner/Banner'
 import MobileBackground from './../../assets/img/banner.png'
+import DesktopBackground from './../../assets/img/desktop-banner.png'
+
 import Card from '../../components/Card/Card'
 
 function Home() {
    return (
       <div className="main-wrapper">
+         <Banner
+            text="Chez vous, partout et ailleurs"
+            background={DesktopBackground}
+         />
+
          <Banner
             text="Chez vous, partout et ailleurs"
             background={MobileBackground}
