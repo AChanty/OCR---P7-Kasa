d9011110349a308accfcab0c2f2c675e0ac3e674
Anthony Chanty <anthony.chanty@hotmail.com>
2023-01-07 20:01:21 +0100

    feat create ResponsiveRules function as utils

---
+++ src/assets/utils/Responsive.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+
+function ResponsiveRules(breakpoint, contentA, contentB) {
+   const [width, setWidth] = useState(window.innerWidth)
+   //    const breakpoint = 992
+   useEffect(() => {
+      const handleResizeWindow = () => setWidth(window.innerWidth)
+      window.addEventListener('resize', handleResizeWindow)
+      return () => {
+         window.removeEventListener('resize', handleResizeWindow)
+      }
+   }, [])
+
+   if (width > breakpoint) {
+      return contentA
+   } else {
+      return contentB
+   }
+}
+
+export default ResponsiveRules
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,58 +1,58 @@
 import './home.css'
 import Banner from '../../components/Banner/Banner'
 import MobileBackground from './../../assets/img/banner.png'
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 import DesktopBackground from './../../assets/img/desktop-banner.png'
 import Card from '../../components/Card/Card'
+import ResponsiveRules from '../../../src/assets/utils/Responsive'
 
 function Home() {
-   const [width, setWidth] = useState(window.innerWidth)
-   const breakpoint = 992
-   useEffect(() => {
-      const handleResizeWindow = () => setWidth(window.innerWidth)
-      window.addEventListener('resize', handleResizeWindow)
-      return () => {
-         window.removeEventListener('resize', handleResizeWindow)
-      }
-   }, [])
+   // const [width, setWidth] = useState(window.innerWidth)
+   // const breakpoint = 992
+   // useEffect(() => {
+   //    const handleResizeWindow = () => setWidth(window.innerWidth)
+   //    window.addEventListener('resize', handleResizeWindow)
+   //    return () => {
+   //       window.removeEventListener('resize', handleResizeWindow)
+   //    }
+   // }, [])
 
-   function responsiveBackground() {
-      if (width > breakpoint) {
-         return (
+   // function responsiveBackground() {
+   //    if (width > breakpoint) {
+   //       return (
+   //          <Banner
+   //             text="Chez vous, partout et ailleurs"
+   //             background={DesktopBackground}
+   //          />
+   //       )
+   //    } else {
+   //       return (
+   //          <Banner
+   //             text="Chez vous, partout et ailleurs"
+   //             background={MobileBackground}
+   //          />
+   //       )
+   //    }
+   // }
+
+   return (
+      <div className="main-wrapper">
+         {ResponsiveRules(
+            992,
+            <Banner
+               text="Chez vous, partout et ailleurs"
+               background={MobileBackground}
+            />,
             <Banner
                text="Chez vous, partout et ailleurs"
                background={DesktopBackground}
             />
-         )
-      } else {
-         return (
-            <Banner
-               text="Chez vous, partout et ailleurs"
-               background={MobileBackground}
-            />
-         )
-      }
-   }
-
-   if (width > breakpoint) {
-      return (
-         <div className="main-wrapper">
-            {responsiveBackground()}
-            <div className="home_cards-container">
-               <Card />
-            </div>
+         )}
+         <div className="home_cards-container">
+            <Card />
          </div>
-      )
-   } else {
-      return (
-         <div className="main-wrapper">
-            {responsiveBackground()}
-            <div className="home_cards-container">
-               <Card />
-            </div>
-         </div>
-      )
-   }
+      </div>
+   )
 }
 
 export default Home
