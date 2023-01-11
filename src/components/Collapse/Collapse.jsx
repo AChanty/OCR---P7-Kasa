804df93d3a0ab27b2a0445fbe7cc53e02041219c
Anthony Chanty <anthony.chanty@hotmail.com>
2023-01-11 16:04:19 +0100

    style Collapse now closed by default

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,21 +1,14 @@
 import { useState } from 'react'
 import './collapse.css'
-// import arrowIcon from './../../assets/icons/arrow.svg'
 import { ReactComponent as ArrowIcon } from './../../assets/icons/arrow.svg'
 
 function Collapse({ headerName, content }) {
-   const [isOpen, isClosed] = useState(true)
+   const [isOpen, isClosed] = useState(false)
 
    return isOpen ? ( // si le composant est ouvert, affiche le contenu qui contient les informations relatives à l'onglet ouvert
       <div className="collapse_item">
          <div className="collapse_head" onClick={() => isClosed(false)}>
             <h3>{headerName}</h3>
-            {/* <img
-               className="collapse_head-closed"
-               //    style={{ transition: '.4s' }}
-               src={arrowIcon}
-               alt="icone flèche"
-            /> */}
             <ArrowIcon className="collapse_head-closed" fill="white" />
          </div>
          <div className="collapse_content">{content}</div>
@@ -24,17 +17,7 @@ function Collapse({ headerName, content }) {
       <div className="collapse_item">
          <div className="collapse_head" onClick={() => isClosed(true)}>
             <h3>{headerName}</h3>
-            {/* <img
-               className="collapse_head-opened"
-               //    style={{ transform: 'rotate(-180deg)', transition: '.4s' }}
-               src={arrowIcon}
-               alt="icone flèche"
-            /> */}
-            <ArrowIcon
-               className="collapse_head-opened"
-               fill="white"
-               //    style={{ transform: 'rotate(-180deg)', transition: '.4s' }}
-            />
+            <ArrowIcon className="collapse_head-opened" fill="white" />
          </div>
 
          {/* change la classe de collapse_content pour pouvoir gérer les animations */}
