102c875027c9a3391f4fcb263000ffb7a8e3dec4
Anthony Chanty <anthony.chanty@hotmail.com>
2023-01-12 23:04:22 +0100

    refactor About to render content from "aboutContent" array

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,6 +4,25 @@ import Image from './../../assets/img/about-background.jpg'
 import MobileImage from './../../assets/img/about-responsive-background.png'
 import ResponsiveRules from '../../../src/assets/utils/Responsive'
 
+const aboutContent = [
+   {
+      header: 'Fiabilité',
+      text: 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.',
+   },
+   {
+      header: 'Respect',
+      text: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+   },
+   {
+      header: 'Service',
+      text: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+   },
+   {
+      header: 'Sécurité',
+      text: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+   },
+]
+
 function About() {
    return (
       <div className="main-wrapper">
@@ -12,9 +31,17 @@ function About() {
             <img className="about_image" src={MobileImage} alt="about" />,
             <img className="about_image" src={Image} alt="about" />
          )}
-         {/* <img className="about_image" src={Image} alt="about" /> */}
          <div className="about_collapses-wrapper">
-            <Collapse
+            {/* parcourt le tableau "aboutContent" pour créer un élément "Collapse" pour chaque entrée présente */}
+            {aboutContent.map((aboutContent, index) => (
+               <Collapse
+                  key={index}
+                  headerName={aboutContent.header}
+                  content={<p>{aboutContent.text}</p>}
+               />
+            ))}
+
+            {/* <Collapse
                headerName="Fiabilité"
                content={
                   <p>
@@ -60,7 +87,7 @@ function About() {
                      la sécurité domestique pour nos hôtes.
                   </p>
                }
-            />
+            /> */}
          </div>
       </div>
    )
