b80e4d2d05e3c1e153e0e9c0139624cde88a01e6
Anthony Chanty <anthony.chanty@hotmail.com>
2023-01-16 22:13:15 +0100

    style clean Card.jsx/Home.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,47 +1,9 @@
-// import { Link } from 'react-router-dom'
-// import './card.css'
-// import Logements from './../../assets/datas/logements.json'
-
-// function Card() {
-//    return (
-//       <div className="card-container">
-//          {Logements.map(
-//             (
-//                logement // créé un div.card-item pour chaque objet disponible dans logement.json
-//             ) => (
-//                <Link
-//                   to={`logement/${logement.id}`} // le lien dirige vers logement/:logement.id
-//                   className="card-item"
-//                   key={logement.id}
-//                >
-//                   <p className="card-title">{logement.title}</p>
-//                   <div className="card-item_img-container">
-//                      <img
-//                         className="card-img"
-//                         src={logement.cover}
-//                         alt={logement.title}
-//                      />
-//                   </div>
-//                </Link>
-//             )
-//          )}
-//       </div>
-//    )
-// }
-
-// export default Card
-
 import { Link } from 'react-router-dom'
 import './card.css'
 
-function Card({ link, id, title, cover }) {
+function Card({ link, title, cover }) {
    return (
-      <Link
-         // to={`/logement/${id}`} // le lien dirige vers logement/:logement.id
-         to={link} // le lien dirige vers logement/:logement.id
-         className="card-item"
-         // key={id}
-      >
+      <Link to={link} className="card-item">
          <p className="card-title">{title}</p>
          <div className="card-item_img-container">
             <img className="card-img" src={cover} alt={title} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,33 +1,3 @@
-// import './home.css'
-// import Banner from '../../components/Banner/Banner'
-// import MobileBackground from './../../assets/img/banner.png'
-// import DesktopBackground from './../../assets/img/desktop-banner.png'
-// import Card from '../../components/Card/Card'
-// import ResponsiveRules from '../../../src/assets/utils/Responsive'
-
-// function Home() {
-//    return (
-//       <div className="main-wrapper">
-//          {ResponsiveRules(
-//             992, // change l'image de fond lorsque l'écran est supérieur à 992px
-//             <Banner
-//                text="Chez vous, partout et ailleurs"
-//                background={DesktopBackground}
-//             />,
-//             <Banner
-//                text="Chez vous, partout et ailleurs"
-//                background={MobileBackground}
-//             />
-//          )}
-//          <div className="home_cards-container">
-//             <Card />
-//          </div>
-//       </div>
-//    )
-// }
-
-// export default Home
-
 import './home.css'
 import Banner from '../../components/Banner/Banner'
 import MobileBackground from './../../assets/img/banner.png'
