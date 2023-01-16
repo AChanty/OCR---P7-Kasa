6b83501772d27f2f96b412061402d811c5189f78
Anthony Chanty <anthony.chanty@hotmail.com>
2023-01-16 16:10:20 +0100

    refactor convert Card component into displaying single Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,31 +1,51 @@
+// import { Link } from 'react-router-dom'
+// import './card.css'
+// import Logements from './../../assets/datas/logements.json'
+
+// function Card() {
+//    return (
+//       <div className="card-container">
+//          {Logements.map(
+//             (
+//                logement // créé un div.card-item pour chaque objet disponible dans logement.json
+//             ) => (
+//                <Link
+//                   to={`logement/${logement.id}`} // le lien dirige vers logement/:logement.id
+//                   className="card-item"
+//                   key={logement.id}
+//                >
+//                   <p className="card-title">{logement.title}</p>
+//                   <div className="card-item_img-container">
+//                      <img
+//                         className="card-img"
+//                         src={logement.cover}
+//                         alt={logement.title}
+//                      />
+//                   </div>
+//                </Link>
+//             )
+//          )}
+//       </div>
+//    )
+// }
+
+// export default Card
+
 import { Link } from 'react-router-dom'
 import './card.css'
-import Logements from './../../assets/datas/logements.json'
 
-function Card() {
+function Card({ id, title, cover }) {
    return (
-      <div className="card-container">
-         {Logements.map(
-            (
-               logement // créé un div.card-item pour chaque objet disponible dans logement.json
-            ) => (
-               <Link
-                  to={`logement/${logement.id}`} // le lien dirige vers logement/:logement.id
-                  className="card-item"
-                  key={logement.id}
-               >
-                  <p className="card-title">{logement.title}</p>
-                  <div className="card-item_img-container">
-                     <img
-                        className="card-img"
-                        src={logement.cover}
-                        alt={logement.title}
-                     />
-                  </div>
-               </Link>
-            )
-         )}
-      </div>
+      <Link
+         to={`/logement/${id}`} // le lien dirige vers logement/:logement.id
+         className="card-item"
+         key={id}
+      >
+         <p className="card-title">{title}</p>
+         <div className="card-item_img-container">
+            <img className="card-img" src={cover} alt={title} />
+         </div>
+      </Link>
    )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,40 @@
+// import './home.css'
+// import Banner from '../../components/Banner/Banner'
+// import MobileBackground from './../../assets/img/banner.png'
+// import DesktopBackground from './../../assets/img/desktop-banner.png'
+// import Card from '../../components/Card/Card'
+// import ResponsiveRules from '../../../src/assets/utils/Responsive'
+
+// function Home() {
+//    return (
+//       <div className="main-wrapper">
+//          {ResponsiveRules(
+//             992, // change l'image de fond lorsque l'écran est supérieur à 992px
+//             <Banner
+//                text="Chez vous, partout et ailleurs"
+//                background={DesktopBackground}
+//             />,
+//             <Banner
+//                text="Chez vous, partout et ailleurs"
+//                background={MobileBackground}
+//             />
+//          )}
+//          <div className="home_cards-container">
+//             <Card />
+//          </div>
+//       </div>
+//    )
+// }
+
+// export default Home
+
 import './home.css'
 import Banner from '../../components/Banner/Banner'
 import MobileBackground from './../../assets/img/banner.png'
-// import { useState, useEffect } from 'react'
 import DesktopBackground from './../../assets/img/desktop-banner.png'
 import Card from '../../components/Card/Card'
 import ResponsiveRules from '../../../src/assets/utils/Responsive'
+import Logements from './../../assets/datas/logements.json'
 
 function Home() {
    return (
@@ -21,7 +51,19 @@ function Home() {
             />
          )}
          <div className="home_cards-container">
-            <Card />
+            <div className="card-container">
+               {Logements.map(
+                  (
+                     logement // créé un div.card-item pour chaque objet disponible dans logement.json
+                  ) => (
+                     <Card
+                        id={logement.id}
+                        title={logement.title}
+                        cover={logement.cover}
+                     />
+                  )
+               )}
+            </div>
          </div>
       </div>
    )
