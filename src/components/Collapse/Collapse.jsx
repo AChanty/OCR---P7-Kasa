b8591ae943439987adaf6668be99e30037bf0881
Anthony Chanty <anthony.chanty@hotmail.com>
2023-02-08 13:37:41 +0100

    refactor Collapse : use ternary operator to set className and component content

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -3,25 +3,32 @@ import './collapse.css'
 import { ReactComponent as ArrowIcon } from './../../assets/icons/arrow.svg'
 
 function Collapse({ headerName, content }) {
-   const [isOpen, isClosed] = useState(false)
+   const [isOpen, setIsOpen] = useState(false)
 
-   return isOpen ? ( // si le composant est ouvert, affiche le contenu qui contient les informations relatives à l'onglet ouvert
+   return (
+      // si le composant est ouvert, affiche le contenu qui contient les informations relatives à l'onglet ouvert
       <div className="collapse_item">
-         <button className="collapse_head" onClick={() => isClosed(false)}>
+         <button className="collapse_head" onClick={() => setIsOpen(!isOpen)}>
             <h3>{headerName}</h3>
-            <ArrowIcon className="collapse_head-closed" fill="white" />
+            <ArrowIcon
+               className={
+                  // modification de la classe par rapport à l'état de "isOpen"
+                  isOpen ? 'collapse_head-closed' : 'collapse_head-opened'
+               }
+               fill="white"
+            />
          </button>
-         <div className="collapse_content">{content}</div>
-      </div>
-   ) : (
-      <div className="collapse_item">
-         <button className="collapse_head" onClick={() => isClosed(true)}>
-            <h3>{headerName}</h3>
-            <ArrowIcon className="collapse_head-opened" fill="white" />
-         </button>
-
-         {/* change la classe de collapse_content pour pouvoir gérer l'animation de transition */}
-         <div className="collapse_content-closed"></div>
+         <div
+            className={
+               // modification de la classe par rapport à l'état de "isOpen"
+               isOpen ? 'collapse_content' : 'collapse_content-closed'
+            }
+         >
+            {isOpen
+               ? /* modification du contenu par rapport à l'état de "isOpen" */
+                 content
+               : null}
+         </div>
       </div>
    )
 }
